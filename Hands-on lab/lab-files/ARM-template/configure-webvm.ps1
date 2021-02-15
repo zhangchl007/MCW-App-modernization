34ea51b18a79c6d8ed39ad2e236bcf2e85b64634
Daron Yondem <daron@yondem.com>
2021-02-15 15:45:49 +0300

    .NET Core 3.1 and .NET 5 SDK Automation Fixes

--- Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
+++ Hands-on lab/lab-files/ARM-template/configure-webvm.ps1
@@ -11,6 +11,28 @@ function Disable-InternetExplorerESC {
     Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
 }
 
+function Wait-Install {
+    $msiRunning = 1
+    $msiMessage = ""
+    while($msiRunning -ne 0)
+    {
+        try
+        {
+            $Mutex = [System.Threading.Mutex]::OpenExisting("Global\_MSIExecute");
+            $Mutex.Dispose();
+            $DST = Get-Date
+            $msiMessage = "An installer is currently running. Please wait...$DST"
+            Write-Host $msiMessage 
+            $msiRunning = 1
+        }
+        catch
+        {
+            $msiRunning = 0
+        }
+        Start-Sleep -Seconds 1
+    }
+}
+
 # To resolve the error of https://github.com/microsoft/MCW-App-modernization/issues/68. The cause of the error is Powershell by default uses TLS 1.0 to connect to website, but website security requires TLS 1.2. You can change this behavior with running any of the below command to use all protocols. You can also specify single protocol.
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
@@ -50,30 +72,36 @@ $action = New-ScheduledTaskAction -Execute "powershell" -Argument $argument
 Register-ScheduledTask -TaskName "Install Lab Requirements" -Trigger $triggerAt -Action $action -User demouser
 
 # Download and install .NET Core 2.2
+Wait-Install
 (New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/5efd5ee8-4df6-4b99-9feb-87250f1cd09f/552f4b0b0340e447bab2f38331f833c5/dotnet-hosting-2.2.2-win.exe', 'C:\dotnet-hosting-2.2.2-win.exe')
 $pathArgs = {C:\dotnet-hosting-2.2.2-win.exe /Install /Quiet /Norestart /Logs logCore22.txt}
 Invoke-Command -ScriptBlock $pathArgs
 
 # Download and install .NET Core 3.1
-(New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/19a5a3cc-b297-4a10-9b22-1184a0aeb990/5af443d748d2c5fb444477f202a11470/dotnet-hosting-3.1.12-win.exe', 'C:\dotnet-hosting-3.1.12-win.exe')
-$pathArgs = {C:\dotnet-hosting-3.1.12-win.exe /Install /Quiet /Norestart /Logs logCore31.txt}
+Wait-Install
+(New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/2fdc3009-cf5c-4cf6-8f3b-a61e83200cbb/2c71ee04b48103a7464f4e28a8bf339b/dotnet-runtime-3.1.12-win-x64.exe', 'C:\dotnet-runtime-3.1.12-win-x64.exe')
+$pathArgs = {C:\dotnet-runtime-3.1.12-win-x64.exe /Install /Quiet /Norestart /Logs logCore31.txt}
 Invoke-Command -ScriptBlock $pathArgs
 
 # Download and install .NET 5 SDK
+Wait-Install
 (New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/674a9f7d-862e-4f92-91b6-f1cf3fed03ce/e07db4de77ada8da2c23261dfe9db138/dotnet-sdk-5.0.103-win-x64.exe', 'C:\dotnet-sdk-5.0.103-win-x64.exe')
 $pathArgs = {C:\dotnet-sdk-5.0.103-win-x64.exe /Install /Quiet /Norestart /Logs logDotNet5.txt}
 Invoke-Command -ScriptBlock $pathArgs
 
 # Download and install SQL Server Management Studio
+Wait-Install
 (New-Object System.Net.WebClient).DownloadFile('https://aka.ms/ssmsfullsetup', 'C:\SSMS-Setup.exe')
 $pathArgs = {C:\SSMS-Setup.exe /Install /Quiet /Norestart /Logs logSSMS.txt}
 Invoke-Command -ScriptBlock $pathArgs
 
 # Install Git
+Wait-Install
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.30.0.windows.2/Git-2.30.0.2-64-bit.exe', 'C:\Git-2.30.0.2-64-bit.exe')
 Start-Process -file 'C:\Git-2.30.0.2-64-bit.exe' -arg '/VERYSILENT /SUPPRESSMSGBOXES /LOG="C:\git_install.txt" /NORESTART /CLOSEAPPLICATIONS' -passthru | wait-process
 
 # Install VS Code
+Wait-Install
 (New-Object System.Net.WebClient).DownloadFile('https://go.microsoft.com/fwlink/?LinkID=623230', 'C:\vscode.exe')
 Start-Process -file 'C:\vscode.exe' -arg '/VERYSILENT /SUPPRESSMSGBOXES /LOG="C:\vscode_install.txt" /NORESTART /FORCECLOSEAPPLICATIONS /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"' -passthru | wait-process
 
